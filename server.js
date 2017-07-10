0e9205385f1baea4fe0a8a41df951b6999cebde6
yarik-vv <yarik.vashchuk@gmail.com>
2017-07-10 07:24:05 +0300

    update server script

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var static = require('node-static');
-var development = new static.Server('./');
+const static = require('node-static');
+const development = new static.Server('./development');
 
 require('http').createServer(function (request, response) {
   if (!/\./.test(request.url)) {
     request.url = '/';
   }
   development.serve(request, response);
-}).listen(3000);
+}).listen(3000, () => {
+  console.log('[goldenbox.org]: Server listening on http://localhost:3000');
+});
